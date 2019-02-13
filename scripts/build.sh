0986e7ef4dca2753629275548d76de91d65be972
Gregory John Casamento <greg.casamento@gmail.com>
2019-02-13 14:58:51 -0500

    Fix issue with using wrong ninja script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@ export NINJA=${ANDROID_CMAKE_ROOT}/bin/ninja
 export ROOT_DIR=`pwd`
 export GSROOT=${ROOT_DIR}/gnustep
 export INSTALL_PREFIX=${ROOT_DIR}/android-gnustep
-export ANDROID_GNUSTEP_INSTALL_ROOT="${GSROOT}"/"${INSTALL_PREFIX}"
+export ANDROID_GNUSTEP_INSTALL_ROOT="${INSTALL_PREFIX}"
 
 cd $ROOT_DIR
 
@@ -36,24 +36,30 @@ ${ANDROID_CMAKE_ROOT}/bin/cmake \
   -DANDROID_NDK=${ANDROID_NDK_HOME} \
   -DCMAKE_LIBRARY_OUTPUT_DIRECTORY="${GSROOT}"/libobjc2/build \
   -DCMAKE_BUILD_TYPE=Debug \
-  -DCMAKE_MAKE_PROGRAM=${ANDROID_CMAKE_ROOT}/bin/ninja \
+  -DCMAKE_MAKE_PROGRAM=${NINJA} \
   -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE} \
   -DANDROID_NATIVE_API_LEVEL=23 \
   -DANDROID_TOOLCHAIN=clang \
   -DCMAKE_INSTALL_PREFIX="${ANDROID_GNUSTEP_INSTALL_ROOT}"
 
-cd ${GSROOT}/libobjc2/build/CMake/
+cd ${GSROOT}/libobjc2/build
 pwd
 
 sed 's/-Wl,--fatal-warnings//' build.ninja > build2.ninja && mv build2.ninja build.ninja
+#sed 's/-stdlib=libc++//g' build.ninja > build2.ninja && mv build2.ninja build.ninja
+
 ${NINJA} -j6
+
+if [ "$?" != "0" ]; then
+    echo "### LIBOBJC2 BUILD FAILED!!!"
+    exit 0
+fi
 
 echo "### Set toolchain vars..."
 export TOOLCHAIN="${ANDROID_NDK_HOME}"/toolchains/llvm/prebuilt/darwin-x86_64
 export CC="${TOOLCHAIN}"/bin/clang
 export CXX="${TOOLCHAIN}"/bin/clang++
 export OBJC="${TOOLCHAIN}"/bin/clang
-#export OBJCC="${TOOLCHAIN}"/bin/clang
 export LD="${CC}"
 export CFLAGS="--gcc-toolchain=${ANDROID_NDK_HOME}/toolchains/llvm/prebuilt/darwin-x86_64 --target=armv7-none-linux-androideabi23"
 export OBJCFLAGS="${CFLAGS}"
@@ -65,7 +71,12 @@ git clone https://github.com/gnustep/tools-make
 cd "${GSROOT}"/tools-make
 ./configure --host=arm-linux-androideabi --prefix="${ANDROID_GNUSTEP_INSTALL_ROOT}" OBJCFLAGS="${OBJCFLAGS} -integrated-as"
 gnumake install
+if [ "$?" != "0" ]; then
+    echo "### MAKE BUILD FAILED!!!"
+    exit 0
+fi
 . "${ANDROID_GNUSTEP_INSTALL_ROOT}"/share/GNUstep/Makefiles/GNUstep.sh
+
 
 echo "### Setup build for base..."
 cd "${GSROOT}"
@@ -86,3 +97,7 @@ configure it
 echo "### Build base..."
 sed 's/cross_objc2_runtime=0/cross_objc2_runtime=1/g' cross.config > cross.config2 && mv cross.config2 cross.config
 gnumake LD="${LD}" LDFLAGS="${LDFLAGS} -nopie" -j6 messages=yes install
+if [ "$?" != "0" ]; then
+    echo "### BASE BUILD FAILED!!!"
+    exit 0
+fi
