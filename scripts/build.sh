7cd7d928b36f9a6e8d5447fc5f513a631a1e12ff
Gregory John Casamento <greg.casamento@gmail.com>
2019-02-16 02:03:02 -0500

    Add tests and new scripts.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+PROJECT=$1
+
 export ROOT_DIR=`pwd`
 export SCRIPT_DIR=${ROOT_DIR}/scripts
 
 . ${SCRIPT_DIR}/sdkenv.sh
 . ${SCRIPT_DIR}/toolchain.sh
+. ${SCRIPT_DIR}/makeenv.sh
 
+cd ${PROJECT}
+make -j6 all
---
+++ scripts/makeenv.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+export ANDROID_SDK=${ANDROID_HOME}
+export COMPILER_BIN=${TOOLCHAIN}/bin/ 
+export JAVA_BIN=/usr/bin
+export GSCONFIG=${INSTALL_PREFIX}/System/Tools/gnustep-config
