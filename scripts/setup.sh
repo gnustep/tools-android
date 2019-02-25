9a1f11b6c25ac455293ffbc3152002a1a9db156c
Gregory John Casamento <greg.casamento@gmail.com>
2019-02-25 15:44:11 -0500

    Correct patch

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -23,7 +23,7 @@ mkdir -p ${INSTALL_PREFIX}/logs
 cd "${SRCROOT}"
 git clone https://github.com/gnustep/libobjc2
 echo "** Applying patches"
-cp ${ROOT_DIR}/*.S libobjc2
+cp ${ROOT_DIR}/patches/*.S libobjc2
 mkdir -p "${SRCROOT}"/libobjc2/build
 
 echo " "
