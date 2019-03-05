0ff8f4b5e37f01679adc0af2cdad33e2c447e3c1
Frederik Seiffert <frederik@algoriddim.com>
2019-03-05 13:08:34 +0100

    Remove creation of makeenv.conf in installation directory.

--- build.sh
+++ build.sh
@@ -25,6 +25,3 @@ for PHASE in "${ROOT_DIR}"/phases/[0-9][0-9]-*.sh; do
     exit $?
   fi
 done
-
-# create makeenv.conf
-perl -pe 's/\${(\w+)}/$ENV{$1}/g' "${ROOT_DIR}"/env/makeenv.sh > "${INSTALL_PREFIX}"/etc/makeenv.conf
