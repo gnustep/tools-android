6a1a4bca345237fadaed968d914ba67f48177a2f
Gregory John Casamento <greg.casamento@gmail.com>
2019-03-15 16:16:46 -0400

    Changes to make sure everything works on Linux

--- env/sdkenv.sh
+++ env/sdkenv.sh
@@ -3,8 +3,13 @@
 echo "### Setting SDK environment"
 
 # Android SDK
-export ANDROID_ROOT=${HOME}/Library/Android
-export ANDROID_HOME=${ANDROID_ROOT}/sdk
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    export ANDROID_ROOT=${HOME}/Android
+    export ANDROID_HOME=${ANDROID_ROOT}/Sdk
+else
+    export ANDROID_ROOT=${HOME}/Library/Android
+    export ANDROID_HOME=${ANDROID_ROOT}/sdk
+fi
 export ANDROID_NDK_HOME=${ANDROID_HOME}/ndk-bundle
 export ANDROID_SYSROOT=${ANDROID_NDK_HOME}/sysroot
 export ANDROID_INCLUDE=${ANDROID_SYSROOT}/usr/include
@@ -24,9 +29,20 @@ export SRCROOT=${ROOT_DIR}/src
 export INSTALL_PREFIX=${ANDROID_ROOT}/GNUstep
 
 # Target (allow overrides)
-export HOST_TAG=${HOST_TAG:-darwin-x86_64}
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    export HOST_TAG=${HOST_TAG:-linux-x86_64}
+else
+    export HOST_TAG=${HOST_TAG:-darwin-x86_64}
+fi
+
 export ABI_NAME=${ABI_NAME:-armeabi-v7a}
 export ANDROID_API_LEVEL=${ANDROID_API_LEVEL:-21}
 export ANDROID_TARGET=${ANDROID_TARGET:-armv7a-linux-androideabi}
 export ANDROID_TARGET_BINUTILS=${ANDROID_TARGET_BINUTILS:-arm-linux-androideabi}
 export BUILD_TYPE=${BUILD_TYPE:-Debug}
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    export MAKE=make
+else
+    export MAKE=gnumake
+fi
