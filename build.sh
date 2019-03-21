ac238e2b5948ae2aa84b5c905f139be1d399c647
Frederik Seiffert <frederik@algoriddim.com>
2019-03-21 11:12:54 +0100

    Keep backup of previous toolchain when building.

--- build.sh
+++ build.sh
@@ -5,10 +5,14 @@ export ROOT_DIR=`pwd`
 
 . "${ROOT_DIR}"/env/sdkenv.sh
 
-# clean up src and installation folders
+# clean up src folder
 rm -rf "${SRCROOT}"
 mkdir -p "${SRCROOT}"
-rm -rf "${INSTALL_PREFIX}"
+
+# keep backup of previous build if any
+if [ -d "${INSTALL_PREFIX}" ]; then
+  mv "${INSTALL_PREFIX}" "${INSTALL_PREFIX}.bak"
+fi
 mkdir -p "${INSTALL_PREFIX}"
 
 # run phases
@@ -22,6 +26,16 @@ for PHASE in "${ROOT_DIR}"/phases/[0-9][0-9]-*.sh; do
   
   if [ $? -ne 0 ]; then
     echo -e "\n### phases/`basename $PHASE` failed"
+    
+    if [ -d "${INSTALL_PREFIX}.bak" ]; then
+      rm -rf "${INSTALL_PREFIX}"
+      mv "${INSTALL_PREFIX}.bak" "${INSTALL_PREFIX}"
+      echo "The previous toolchain build has been restored."
+    fi
+    
     exit $?
   fi
 done
+
+# remove backup if all went well
+rm -rf "${INSTALL_PREFIX}.bak"
