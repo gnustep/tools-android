875e3717c0ed116d4d7c549de434941c57d25bc2
Frederik Seiffert <frederik@algoriddim.com>
2020-04-15 11:05:41 +0200

    libobjc2: use "libc++" branch
    Enables using libc++abi:
    https://github.com/gnustep/libobjc2/pull/152

--- phases/11-libobjc2.sh
+++ phases/11-libobjc2.sh
@@ -4,7 +4,7 @@ set -e # make any subsequent failing command exit the script
 
 . `dirname $0`/common.sh
 
-prepare_project "libobjc2" "https://github.com/gnustep/libobjc2.git" "libc++-hack"
+prepare_project "libobjc2" "https://github.com/gnustep/libobjc2.git" "libc++"
 
 . "${ROOT_DIR}"/env/toolchain.sh
 
