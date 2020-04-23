7bfb96a106170ae462ba43f6e0da36e6d829f8b0
Frederik Seiffert <frederik@algoriddim.com>
2020-04-23 11:17:39 +0200

    libobjc2: remove exception debug logs

--- phases/11-libobjc2.sh
+++ phases/11-libobjc2.sh
@@ -16,7 +16,6 @@ ${CMAKE} .. \
   ${CMAKE_OPTIONS} \
   -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
   -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY="BOTH" \
-  -DCMAKE_C_FLAGS="-DDEBUG_EXCEPTIONS=1" `# debug exception throwing` \
   -DGNUSTEP_CONFIG= `# prevent cmake from finding gnustep-config in install root` \
   -DOLDABI_COMPAT=false `# we're using gnustep-2.0 ABI, which may not be mixed with earlier versions'` \
 
