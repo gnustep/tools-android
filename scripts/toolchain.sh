0fdce4bef1c3232d23dee51f9ddbb3fef3991f5a
Frederik Seiffert <frederik@algoriddim.com>
2023-12-04 15:40:52 +0100

    gnustep-make: enable optimizations and always generate debug info
    Fixes #18.

--- scripts/toolchain.sh
+++ scripts/toolchain.sh
@@ -20,13 +20,18 @@ export NM="${TOOLCHAIN}"/bin/llvm-nm
 export OBJDUMP="${TOOLCHAIN}"/bin/llvm-objdump
 export PKG_CONFIG_PATH="${INSTALL_PREFIX}/lib/pkgconfig"
 
+# always generate debug info and build with optimizations
+if [ ! -n "${OPTFLAG+set}" ]; then
+  OPTFLAG="-g -O2"
+fi
+
 # NOTE: The following compiler and linker flags mirror the NDK's CMake toolchain file
 # and are recommended by the Android Build System Maintainers Guide (see link above)
 
 # - emit stack guards to protect against security vulnerabilities caused by buffer overruns
 # - enable FORTIFY to try to catch incorrect use of standard functions
 # - generate position-independent code (PIC) to remove unsupported text relocations
-export CFLAGS="-fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIC"
+export CFLAGS="$OPTFLAG -fstack-protector-strong -D_FORTIFY_SOURCE=2 -fPIC"
 
 # -L library search path required for some projects to find libraries (e.g. gnustep-corebase)
 # -fuse-ld=lld require to enforce LLD, which is needed e.g. for --no-rosegment flag
